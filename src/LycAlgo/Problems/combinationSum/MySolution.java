cb0070d06404a5967452b0cc7898842e7e1a2b29
windlikelyc <windlikelyc@gmail.com>
2017-09-13 16:50:47 +0800

    回溯法：穷举所有可能的解且每种解只尝试一次

---
+++ src/LycAlgo/Problems/combinationSum/MySolution.java
@@ -0,0 +1,20 @@
+package LycAlgo.Problems.combinationSum;
+
+import java.util.List;
+
+/**
+ * Created by lyc on 2017/9/13.
+ */
+public class MySolution {
+
+    //Find all possible combinations of k numbers that add up to a number n,
+    // given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.
+    public List<List<Integer>> combinationSum3(int k, int n) {
+
+
+        return null;
+
+    }
+
+
+}
