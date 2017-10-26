086cf0c01b16afeb0023e53ee9d8e74f5a3c4f63
lyc <windlikelyc@gmail.com>
2017-10-26 21:49:47 +0800

    init project in laptop

---
+++ src/LycAlgo/Problems/differentWayToAddParentness/Solution.java
@@ -0,0 +1,43 @@
+package LycAlgo.Problems.differentWayToAddParentness;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public List<Integer> diffWaysToCompute(String input,int dep ) {
+        List<Integer> res = new ArrayList<Integer>();
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c == '-' || c == '+' || c == '*') {
+                String a = input.substring(0, i);
+                String b = input.substring(i + 1);
+                List<Integer> al = diffWaysToCompute(a,dep+1);
+                System.out.println(al+""+dep);
+                List<Integer> bl = diffWaysToCompute(b,dep+1);
+                System.out.println(bl+""+dep);
+                for (int x : al) {
+                    for (int y : bl) {
+                        if (c == '-') {
+                            res.add(x - y);
+                        } else if (c == '+') {
+                            res.add(x + y);
+                        } else if (c == '*') {
+                            res.add(x * y);
+                        }
+                    }
+                }
+            }
+        }
+        if (res.size() == 0) res.add(Integer.valueOf(input));
+        return res;
+    }
+
+
+    @Test
+    public void test(){
+        diffWaysToCompute("1+2*3+4",0);
+    }
+}
---
+++ src/LycAlgo/Problems/kthsmallestMartrix/Solution.java
@@ -0,0 +1,22 @@
+package LycAlgo.Problems.kthsmallestMartrix;
+
+import LycAlgo.utils.ListModule.List;
+
+import java.util.PriorityQueue;
+
+public class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0; i < matrix.length ; i++) {
+            for(int j = 0 ; j < matrix[0].length ; j++) {
+                pq.add(matrix[i][j]);
+            }
+        }
+
+        for(int i = 0 ; i< k-1 ; i++) {
+            pq.poll();
+        }
+        return pq.poll();
+    }
+
+}
