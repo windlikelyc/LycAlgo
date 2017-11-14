8379199f6f4403d6efad58aff4e9643ae6ea03f4
windlikelyc <windlikelyc@gmail.com>
2017-11-14 16:25:09 +0800

    213

---
+++ src/LycAlgo/Problems/Permunation2/Solution.java
@@ -0,0 +1,51 @@
+package LycAlgo.Problems.Permunation2;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lyc on 2017/11/9.
+ */
+public class Solution {
+
+    List<Integer> allnums = new ArrayList<>();
+    List<List<Integer>> ans = new ArrayList<>();
+
+    // 1 ,2 ,3
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        for(int i = 0 ; i < nums.length ; i++) {
+            allnums.add(nums[i]);
+        }
+        //下标数组，0为未使用，1为已使用
+        int[] index = new int[nums.length];
+        List<Integer> list = new ArrayList();
+        fillthislist(list, allnums,nums,index);
+        return ans;
+    }
+
+    private void fillthislist(List<Integer> nowlist, List<Integer> allnums,int[] nums,int[] index) {
+
+        if(nowlist.size() == nums.length){
+            ans.add(new ArrayList<Integer>(nowlist));
+            return;
+        }
+
+        for(int i = 0 ; i < nums.length ; i++) {
+            if(index[i] == 1){
+                continue;
+            }
+            nowlist.add(nums[i]);
+            index[i] = 1;
+            fillthislist(nowlist, allnums,nums,index);
+            index[i] = 0;
+            nowlist.remove(nums[i]);
+        }
+    }
+
+    @Test
+    public void test(){
+        System.out.print(permuteUnique(new int[]{1,2,3}));
+    }
+}
