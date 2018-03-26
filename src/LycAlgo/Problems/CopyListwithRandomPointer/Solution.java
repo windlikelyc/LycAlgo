7ec1f3fac17724962494a839089e3511c8a8632d
windlikelyc <windlikelyc@gmail.com>
2018-03-26 18:08:12 +0800

    combination sum

---
+++ src/LycAlgo/Problems/CopyListwithRandomPointer/Solution.java
@@ -0,0 +1,16 @@
+package LycAlgo.Problems.CopyListwithRandomPointer;
+
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        return null;
+    }
+
+    class RandomListNode {
+        int label;
+        RandomListNode next, random;
+
+        RandomListNode(int x) {
+            this.label = x;
+        }
+    }
+}
--- src/LycAlgo/sort/HeapSort.java
+++ src/LycAlgo/sort/HeapSort.java
@@ -4,35 +4,37 @@ import java.util.Arrays;
 
 public class HeapSort {
 
-    public static void adjustHeap(int[] a, int i, int len) {
-        int temp, j;
-        temp = a[i];
-        for (j = 2 * i; j < len; j *= 2) {// 沿关键字较大的孩子结点向下筛选
-            if (j < len && a[j] < a[j + 1])
+    public static void adjustHeap(int[] a, int index, int len) {
+        int temp = a[index];
+        for (int j = 2 * index + 1; j < len; j = 2 * j + 1) {// 沿关键字较大的孩子结点向下筛选
+            if (j + 1 < len && a[j] < a[j + 1])
                 ++j; // j为关键字中较大记录的下标
-            if (temp >= a[j])
+            if (a[j] > temp) {
+                a[index] = a[j];
+                index = j;
+            } else {
                 break;
-            a[i] = a[j];
-            i = j;
+            }
         }
-        a[i] = temp;
+        a[index] = temp;
     }
 
     public static void heapSort(int[] a) {
         int i;
         for (i = a.length / 2 - 1; i >= 0; i--) {// 构建一个大顶堆
-            adjustHeap(a, i, a.length - 1);
+            adjustHeap(a, i, a.length);
         }
-        for (i = a.length - 1; i >= 0; i--) {// 将堆顶记录和当前未经排序子序列的最后一个记录交换
+        for (i = a.length - 1; i > 0; i--) {// 将堆顶记录和当前未经排序子序列的最后一个记录交换
             int temp = a[0];
             a[0] = a[i];
             a[i] = temp;
-            adjustHeap(a, 0, i - 1);// 将a中前i-1个记录重新调整为大顶堆
+            adjustHeap(a, 0, i);// 将a中前i-1个记录重新调整为大顶堆
         }
     }
 
     public static void main(String[] args) {
-        int[] A = new int[]{2, 1, 3, 4, 12, 45, 543, 32, 12, 5, 3, 6, 1, 2, 8, 7};
+        int[] A = new int[]{0, 1, 2, 3, 4, 5, 6};
+//        int[] A = new int[]{2, 1, 3};
         heapSort(A);
         System.out.println(Arrays.toString(A));
     }
