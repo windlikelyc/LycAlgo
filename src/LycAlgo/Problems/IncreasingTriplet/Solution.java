3f762d4a59a27ad23e5085a603ecc6e89bda50fc
windlikelyc <windlikelyc@gmail.com>
2018-03-26 15:13:13 +0800

    123

---
+++ src/LycAlgo/Problems/IncreasingTriplet/Solution.java
@@ -0,0 +1,24 @@
+package LycAlgo.Problems.IncreasingTriplet;
+
+import com.sleepycat.je.tree.IN;
+
+import java.util.Stack;
+
+public class Solution {
+
+//    class Solution {
+//        public:
+//        bool increasingTriplet(vector<int>& nums) {
+//            int n = nums.size();
+//            if (n < 3) return false;
+//            int a = INT_MAX, b = INT_MAX;
+//            for (int i = 0; i < n; ++i)
+//            {
+//                if (nums[i] <= a) a = nums[i];
+//                else if (nums[i] <= b) b = nums[i];
+//                else return true;
+//            }
+//            return false;
+//        }
+//    };
+}
